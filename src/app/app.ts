470b9fbf33803f930482033c15120c510906efef
Nelson Lamprecht <14348300+NelsonLamprecht@users.noreply.github.com>
2026-03-31 22:39:57 +0000

    adds bootstrap and minimum age settings

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,7 +4,7 @@ import { Component, signal } from '@angular/core';
   selector: 'app-root',
   imports: [],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('kayzaapp');
